9662107bdee0bc53563af4aba786e20298fe35d1
abhisheksingh98 <writetoabhishek07@gmail.com>
2020-09-14 04:35:05 +0530

    added props to product

--- src/Product.js
+++ src/Product.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import './product.css'
+import './Product.css'
 
-function Product() {
+function Product({title,image,rating,price}) {
     return (
         <div className="product">
-            
+            <div className="product__info">
+            <p>{title}</p>
+            <p className="product__price">
+                <small>₹ </small>
+                <strong>{price}</strong>
+            </p>
+            <div className="product__rating">
+            {Array(rating).fill().map((_,i)=>( <p>⭐</p>))}
+           
+            <p>🌟</p>
+            <p>🌟</p>
+            </div>
+            </div>
+            <img src="https://images-na.ssl-images-amazon.com/images/I/71aFt4+OTOL.jpg" alt=""/>
+
+            <button>Add to Cart</button>
         </div>
     )
 }
