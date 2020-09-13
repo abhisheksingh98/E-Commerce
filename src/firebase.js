b538f11fe6e2a21644f29b74b2c2c04188749dbc
abhisheksingh98 <writetoabhishek07@gmail.com>
2020-09-14 02:22:11 +0530

    added navbar, amazon logo, search bar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCPlRc2g06opt_x-Un2gFHbsttgywS1-zA",
+    authDomain: "clone-bfeca.firebaseapp.com",
+    databaseURL: "https://clone-bfeca.firebaseio.com",
+    projectId: "clone-bfeca",
+    storageBucket: "clone-bfeca.appspot.com",
+    messagingSenderId: "6807476513",
+    appId: "1:6807476513:web:e828ca51048d0e9f15c660",
+    measurementId: "G-C2M70DNQDX"
+  };
