b49fe6aaabef31b94fcede32271c6acaaf5a419b
abhisheksingh98 <writetoabhishek07@gmail.com>
2020-09-14 03:42:43 +0530

    added body image and added some styles, also added the product component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // import logo from './logo.svg';
 import Header from './Header';
+import Home from './Home'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Home/>
     </div>
   );
 }
