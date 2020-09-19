ead0a1844983b699bee69cf0ce9358d413d408f9
abhisheksingh98 <writetoabhishek07@gmail.com>
2020-09-20 03:31:01 +0530

    added state provider and reducer, used context api

--- src/Product.js
+++ src/Product.js
@@ -2,6 +2,22 @@ import React from 'react'
 import './Product.css'
 
 function Product({id,title,price,rating,image}) {
+
+    const [state,dispatch] = useStateValue();
+    const addToBasket =() =>{
+        //dispatch them item into the data layer
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item : {
+                id : id,
+                title : title,
+                image: image,
+                price : price,
+                rating : rating
+            }
+        })
+    }
+
     return (
         <div className="product">
             <div className="product__info">
@@ -16,7 +32,7 @@ function Product({id,title,price,rating,image}) {
             </div>
             <img src={image} alt=""/>
 
-            <button>Add to Cart</button>
+            <button onClick = {addToBasket} >Add to Cart</button>
         </div>
     )
 }
